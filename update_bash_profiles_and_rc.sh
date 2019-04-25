0b89648be918393e2e54a895ef3193f3f163ab4a
jdfraine <jdfraine@gmail.com>
2019-04-25 15:49:02 +0300

    using github instead of ssh to update drones

--- update_bash_profiles_and_rc.sh
+++ update_bash_profiles_and_rc.sh
@@ -1,11 +1,15 @@
-rsync -Pvua /home/acc/.bash* 172.16.50.181:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.176:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.177:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.163:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.182:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.218:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.235:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.159:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.157:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.237:~/
-rsync -Pvua /home/acc/.bash* 172.16.50.187:~/
+git add -u
+git commit -m $1
+git push
+
+echo 172.16.50.181; ssh 172.16.50.181 cd vaelstmpredictor/; git pull
+echo 172.16.50.176; ssh 172.16.50.176 cd vaelstmpredictor/; git pull
+echo 172.16.50.177; ssh 172.16.50.177 cd vaelstmpredictor/; git pull
+echo 172.16.50.163; ssh 172.16.50.163 cd vaelstmpredictor/; git pull
+echo 172.16.50.182; ssh 172.16.50.182 cd vaelstmpredictor/; git pull
+echo 172.16.50.218; ssh 172.16.50.218 cd vaelstmpredictor/; git pull
+echo 172.16.50.235; ssh 172.16.50.235 cd vaelstmpredictor/; git pull
+echo 172.16.50.159; ssh 172.16.50.159 cd vaelstmpredictor/; git pull
+echo 172.16.50.157; ssh 172.16.50.157 cd vaelstmpredictor/; git pull
+echo 172.16.50.237; ssh 172.16.50.237 cd vaelstmpredictor/; git pull
+echo 172.16.50.187; ssh 172.16.50.187 cd vaelstmpredictor/; git pull
