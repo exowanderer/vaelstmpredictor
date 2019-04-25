cb1ff20f1c92e0782da8951b151a2a5fab08c42c
jdfraine <jdfraine@gmail.com>
2019-04-25 15:50:49 +0300

    updated deleteme

--- update_bash_profiles_and_rc.sh
+++ update_bash_profiles_and_rc.sh
@@ -1,7 +1,3 @@
-git add -u
-git commit -m $1
-git push
-
 echo 172.16.50.181; ssh 172.16.50.181 cd vaelstmpredictor/; git pull
 echo 172.16.50.176; ssh 172.16.50.176 cd vaelstmpredictor/; git pull
 echo 172.16.50.177; ssh 172.16.50.177 cd vaelstmpredictor/; git pull
@@ -12,4 +8,4 @@ echo 172.16.50.235; ssh 172.16.50.235 cd vaelstmpredictor/; git pull
 echo 172.16.50.159; ssh 172.16.50.159 cd vaelstmpredictor/; git pull
 echo 172.16.50.157; ssh 172.16.50.157 cd vaelstmpredictor/; git pull
 echo 172.16.50.237; ssh 172.16.50.237 cd vaelstmpredictor/; git pull
-echo 172.16.50.187; ssh 172.16.50.187 cd vaelstmpredictor/; git pull
+echo 172.16.50.187; ssh 172.16.50.187 cd github/vaelstmpredictor/; git pull
