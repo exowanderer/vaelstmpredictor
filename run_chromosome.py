e937c8f7e7609af5e162adf8bf4d943930927fc3
jdfraine <jdfraine@gmail.com>
2019-04-25 16:38:39 +0300

    typo: order is important

--- run_chromosome.py
+++ run_chromosome.py
@@ -1,18 +1,18 @@
 # from https://github.com/philippesaade11/vaelstmpredictor/blob/GeneticAlgorithm/Genetic-Algorithm.py
 # python vaelstmpredictor/genetic_algorithm_vae_predictor.py ga_vae_nn_test_0 --verbose --iterations 500 --population_size 10 --num_epochs 200
 import argparse
-import socket
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import requests
-
-from time import time, sleep
-import json
-
-from vaelstmpredictor.utils.data_utils import MNISTData
+import socket
 
 from GeneticAlgorithm import *
+
+from time import time, sleep
+from vaelstmpredictor.utils.data_utils import MNISTData
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
