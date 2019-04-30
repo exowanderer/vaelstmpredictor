45666110c3ee00807209a8ff258754087224bda2
jdfraine <jdfraine@gmail.com>
2019-04-30 22:06:41 +0300

    fixing typos after generation loop

--- genetic_algorithm_vae_predictor_Multi_Computer.py
+++ genetic_algorithm_vae_predictor_Multi_Computer.py
@@ -142,15 +142,16 @@ if __name__ == '__main__':
 	# evolutionary_tree = {}
 	# evolutionary_tree[generationID] = save_generation_to_tree(generation,
 	# 														verbose=verbose)
-
+	best_fitness = []
 	fitnesses = [chrom[1].fitness for chrom in generation.iterrows()]
-	best_fitness = [max(fitnesses)]
+	new_best_fitness = max(fitnesses)
 	
 	if clargs.verbose:
 		print('[INFO] For Generation: {}, the best fitness was {}'.format(
 				generationID, new_best_fitness))
 
 	best_fitness.append(new_best_fitness)
+	
 	if make_plots:
 		fig = plt.gcf()
 		fig.show()
