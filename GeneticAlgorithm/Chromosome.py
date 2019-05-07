8f5c3c0728f5d312e7a67ff6ffed9f37603e23cb
jdfraine <jdfraine@gmail.com>
2019-05-07 14:18:23 +0300

    updated genetic_algorith to use vae_predictor.dense

--- GeneticAlgorithm/Chromosome.py
+++ GeneticAlgorithm/Chromosome.py
@@ -17,7 +17,7 @@ from vaelstmpredictor.utils.model_utils import save_model_in_pieces
 from vaelstmpredictor.utils.model_utils import AnnealLossWeight
 from vaelstmpredictor.utils.data_utils import MNISTData
 from vaelstmpredictor.utils.weightnorm import data_based_init
-from vaelstmpredictor.vae_predictor.model import VAEPredictor
+from vaelstmpredictor.vae_predictor.dense_model import VAEPredictor
 from vaelstmpredictor.vae_predictor.train import train_vae_predictor
 
 class Chromosome(VAEPredictor):
