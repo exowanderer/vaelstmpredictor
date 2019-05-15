ae37df51ebc4dd0de20798f0bed23fd821d0da05
jdfraine <jdfraine@gmail.com>
2019-05-15 15:24:37 +0300

    deactivated send_back

--- examples/run_test_genetic_algorithm_vae_predictor_multi_computer.sh
+++ examples/run_test_genetic_algorithm_vae_predictor_multi_computer.sh
@@ -10,8 +10,8 @@ python genetic_algorithm_vae_predictor_multi_computer.py \
 	--population_size 10 \
 	--verbose \
 	--do_log \
-	--do_ckpt \
-	--send_back | ssh 172.16.50.163 "tee -a /home/acc/github/vaelstmpredictor/examples/output_ga_run_172.16.50.187.txt"
+	--do_ckpt | ssh 172.16.50.163 "tee -a /home/acc/github/vaelstmpredictor/examples/output_ga_run_172.16.50.187.txt"
 
+# --send_back 
 # | tee output.file
 # rsync -Pvua output.file 172.16.50.163:/home/acc/github/vaelstmpredictor/examples/
